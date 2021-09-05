0984a4e85e815e8508e2209bb35c8a12798d120b
Rebecca Risha <rebecca.risha@ithands.com>
2021-09-05 19:11:09 +0530

    class component example

--- demo/src/App.js
+++ demo/src/App.js
@@ -1,9 +1,11 @@
 import "./App.css";
 import Greet from "./Greet.js";
+import Welcome from "./Welcome.js";
 function App() {
   return (
     <div className="App">
       <Greet></Greet>
+      <Welcome> </Welcome>
       Learn React
     </div>
   );
