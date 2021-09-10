89abbb03cc48fea4a9de702322b43b82fe0cd0f2
Rebecca Risha <rebecca.risha@ithands.com>
2021-09-10 20:08:19 +0530

    passign event handler as a prop from parent to child

--- demo/src/PropsExample/Child.js
+++ demo/src/PropsExample/Child.js
@@ -2,11 +2,12 @@ import React, { Component } from "react";
 
 class Child extends Component {
   render() {
-    const { name } = this.props;
+    const { name, job } = this.props;
     return (
       <div>
         {this.props.name}
         {name}
+        <button onClick={job}>CLICK ME (CHILD)</button>
       </div>
     );
   }
--- demo/src/PropsExample/Parent.js
+++ demo/src/PropsExample/Parent.js
@@ -1,10 +1,20 @@
 import React, { Component } from "react";
 import Child from "./Child.js";
 class Parent extends Component {
+  constructor() {
+    super();
+    this.getJobProfile = this.getJobProfile.bind(this);
+    this.state = {
+      className: "Jeetendra",
+    };
+  }
+  getJobProfile() {
+    alert(`message from parent class name ${this.state.className}`);
+  }
   render() {
     return (
       <div>
-        <Child name="risha"></Child>
+        <Child name="risha" job={this.getJobProfile}></Child>
       </div>
     );
   }
