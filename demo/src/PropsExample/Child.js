f04ccfb570c5743352780c103aecb1aa71c16ac7
Rebecca Risha <rebecca.risha@ithands.com>
2021-09-11 08:31:29 +0530

    passing prop from child to parent but this time changing the state of parent variable

--- demo/src/PropsExample/Child.js
+++ demo/src/PropsExample/Child.js
@@ -1,13 +1,15 @@
 import React, { Component } from "react";
 
+
 class Child extends Component {
   render() {
-    const { name, job } = this.props;
+    const { name, job, status } = this.props;
     return (
       <div>
         {this.props.name}
         {name}
         <button onClick={() => job("child name")}>CLICK ME (CHILD)</button>
+        <button onClick={() => status("passed status")}>P</button>
       </div>
     );
   }
--- demo/src/PropsExample/Parent.js
+++ demo/src/PropsExample/Parent.js
@@ -6,17 +6,30 @@ class Parent extends Component {
     this.getJobProfile = this.getJobProfile.bind(this);
     this.state = {
       className: "Parent",
+      status: "before status",
     };
+    this.changeStatus = this.changeStatus.bind(this);
   }
   getJobProfile(child) {
     alert(
       `message from parent class name ${this.state.className} to  ${child}`
     );
   }
+  changeStatus(data) {
+    this.setState({
+      status: data,
+    });
+  }
+
   render() {
     return (
       <div>
-        <Child name="risha" job={this.getJobProfile}></Child>
+        <Child
+          name="risha"
+          job={this.getJobProfile}
+          status={this.changeStatus}
+        ></Child>
+        {this.state.status}
       </div>
     );
   }
