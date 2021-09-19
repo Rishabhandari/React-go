69de609b05bb9edb9c9aeb419010ae8e446df5b7
Rebecca Risha <rebecca.risha@ithands.com>
2021-09-19 22:43:29 +0530

    using context for passing prop to child component without passing to parent

--- demo/src/App.js
+++ demo/src/App.js
@@ -1,12 +1,14 @@
 import "./App.css";
-import CounterClick from "./HigherOrderComponents/CounterClick";
-import CounterMouseHover from "./HigherOrderComponents/CounterMouseHover";
+
+// import CounterClick from "./HigherOrderComponents/CounterClick";
+// import CounterMouseHover from "./HigherOrderComponents/CounterMouseHover";
 // import ParentHeroComponent from "./ErroBoundaries/ParentHeroComponent";
 // import Form from "./FormHandling/Form";
 // import LifeCycleComponentA from "./Lifecycle/LifeCycleComponentA";
 // import Example from "./ConditionalOperators/Example";
 // import ExampleForEach from "./Loops/ExampleForEach";
-// import Parent from "./PropsExample/Parent.js";
+import Parent from "./PropsExample/Parent.js";
+import { UserProvider } from "./PropsExample/useContext";
 // import Greet from "./Greet.js";
 // import Welcome from "./Welcome.js";
 function App() {
@@ -14,16 +16,17 @@ function App() {
     <div className="App">
       {/* <Greet name="risha"></Greet>
       <Welcome name="amit"> </Welcome> */}
-      {/* <Parent></Parent> */}
+      <UserProvider value="Risha">
+       <Parent/>
+      </UserProvider>
       Learn React
       {/* <Example /> */}
       {/* <ExampleForEach /> */}
       {/* <Form />
       <LifeCycleComponentA /> */}
       {/* <ParentHeroComponent /> */}
-      <CounterClick />
-      <h1>Counter Mouse Hover</h1>
-      <CounterMouseHover/>
+      {/* <CounterClick /> */}
+      {/* <CounterMouseHover/> */}
     </div>
   );
 }
--- demo/src/PropsExample/Child.js
+++ demo/src/PropsExample/Child.js
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import { UserConsumer } from "./useContext";
 
 
 class Child extends Component {
@@ -10,6 +11,17 @@ class Child extends Component {
         {name}
         <button onClick={() => job("child name")}>CLICK ME (CHILD)</button>
         <button onClick={() => status("passed status")}>P</button>
+
+        {/* using context since parent component is provider and its sub classes are consumenr 
+        So , context helps for passing child prop without passing it to its parent component
+        */}
+        <UserConsumer>
+
+          {username => { 
+            return <h1>uihiuh{username}</h1>
+          }
+          }
+        </UserConsumer>
       </div>
     );
   }
